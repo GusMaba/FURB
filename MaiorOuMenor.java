debe79fc302c7fa32a223edd5d6b62e8469922e3
GusMaba <gusmaba@furb.br>
2026-08-20 20:43:34 -0300

    Add files via upload

---
+++ MaiorOuMenor.java
@@ -0,0 +1,22 @@
+﻿import java.util.Scanner;
+
+public class MaiorOuMenor {
+
+    public static void main(String[] args) {
+        Scanner tec = new Scanner(System.in);
+        
+        int a, b;
+
+        a = tec.nextInt();
+        b = tec.nextInt();
+        if (a > b) {
+            System.out.println(a + " é Maior que " + b);
+        } else {
+            if (a == b) {
+                System.out.println("Ambos são iguais");
+            } else {
+                System.out.println( b + " é Maior que " + a);
+            }
+        }
+    }
+}
---
+++ ParOuImpar.java
@@ -0,0 +1,17 @@
+﻿import java.util.Scanner;
+
+public class ParOuImpar {
+
+    public static void main(String[] args) {
+        Scanner tec = new Scanner(System.in);
+
+        int numero;
+
+        numero = tec.nextInt();
+        if (numero % 2 == 0) {
+            System.out.println("O número é Par");
+        } else {
+            System.out.println("O número é Impar");
+        }
+    }
+}
