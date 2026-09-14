ac049352a43d140184ccb33487d6f05a58dd2879
GiancarloCattoni123 <giancarlocattoni2007@gmail.com>
2026-09-13 21:25:39 -0300

    Update Main1038.java

--- ListaBeeCrowd2/Main1038.java
+++ ListaBeeCrowd2/Main1038.java
@@ -1,37 +1,34 @@
-
 import java.util.Scanner;
 
 public class Main1038 {
     public static void main(String[] args) {
-        Scanner sc= new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
 
         int codigo;
-        float valor,quantidade;
+        float valor, quantidade;
 
         codigo = sc.nextInt();
         quantidade = sc.nextFloat();
 
-        if(codigo == 1){
+        if (codigo == 1) {
             valor = quantidade * 4;
-            System.out.println("Total: R$ "+ valor);
+            System.out.printf("Total: R$ %.2f%n", valor);
         }
-        else if(codigo == 2){
+        else if (codigo == 2) {
             valor = quantidade * 4.50f;
-            System.out.println("Total: R$ "+ valor);
+            System.out.printf("Total: R$ %.2f%n", valor);
         }
-        else if(codigo == 3){
+        else if (codigo == 3) {
             valor = quantidade * 5;
-            System.out.println("Total: R$ "+ valor);
+            System.out.printf("Total: R$ %.2f%n", valor);
         }
-        else if(codigo == 4){
+        else if (codigo == 4) {
             valor = quantidade * 2;
-            System.out.println("Total: R$ "+ valor);
+            System.out.printf("Total: R$ %.2f%n", valor);
         }
-        else if(codigo == 5){
+        else if (codigo == 5) {
             valor = quantidade * 1.50f;
-            System.out.println("Total: R$ "+ valor);
+            System.out.printf("Total: R$ %.2f%n", valor);
         }
-
-
     }
 }
